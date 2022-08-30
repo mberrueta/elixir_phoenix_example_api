43dd08245a826726191439c2c60a385905ab5403
mberrueta <matiasberrueta@gmail.com>
2022-08-30 06:01:27 -0300

    Added credo

--- lib/imdb/mailer.ex
+++ lib/imdb/mailer.ex
@@ -1,3 +1,5 @@
 defmodule Imdb.Mailer do
+  @moduledoc false
+
   use Swoosh.Mailer, otp_app: :imdb
 end
--- lib/imdb_web/telemetry.ex
+++ lib/imdb_web/telemetry.ex
@@ -1,4 +1,6 @@
 defmodule ImdbWeb.Telemetry do
+  @moduledoc false
+
   use Supervisor
   import Telemetry.Metrics
 
